25c49b0caf67088ce5936abc8f4e92f71f0277e4
Pablo Sanchez Bello <pablo.sanchez_bello@mercedes-benz.com>
2023-07-27 11:10:57 +0200

    Initial setup of microservices project

---
+++ flight-service/src/test/java/com/mercedesbenz/flightservice/FlightServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercedesbenz.flightservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
